fa2b7ed12445bcfd2049b26afb428dc3a3f9c83a
Premshaw23 <shawprem217@gmail.com>
2026-01-23 14:45:06 +0530

    update navbar

--- components/Nav/Navbar.js
+++ components/Nav/Navbar.js
@@ -109,7 +109,7 @@ export default function PremiumNavbar() {
         />
 
         <div className="max-w-7xl mx-auto px-6 sm:px-8">
-          <div className="flex items-center justify-between h-20">
+          <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <Link href="/" className="flex items-center gap-3 group">
               <motion.div 
